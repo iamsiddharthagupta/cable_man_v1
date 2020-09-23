1b24d579343c221564c8bd6ae7f1779f4ad651bf
Siddhartha Gupta <32353198+sidmustdie@users.noreply.github.com>
2020-09-24 00:03:30 +0530

    Changes

--- admin_change_pass.php
+++ admin_change_pass.php
@@ -3,17 +3,33 @@
 	$result = $admin->admin_change_pass($curr_user);
 ?>
 
-		<div class="col-md-9">
-			<form method="POST" autocomplete="off">
-				<label>Current Password:</label>
-				<input type="password" name="current_pass" class="form-control" placeholder="Enter your current password">
-				<label>New Password:</label>
-				<input type="password" name="new_pass" class="form-control" placeholder="Enter your new password">
-				<label>Confirm Password:</label>
-				<input type="password" name="confirm_pass" class="form-control mb-2" placeholder="Confirm your new password">
-				<button type="submit" name="submit" class="btn btn-outline-primary">Submit</button>
-			</form>
+	<div class="col-md-9">
+		<div class="card card-primary">
+	        <div class="card-header">
+	          <h3 class="card-title">Change Password</h3>
+	          <div class="card-tools">
+	            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+	              <i class="fas fa-minus"></i></button>
+	          </div>
+	        </div>
+	        <div class="card-body">
+				<form method="POST" autocomplete="off">
+					<div class="form-group">
+						<input type="password" name="current_pass" class="form-control" placeholder="Enter your current password">
+					</div>
+					<div class="form-group">
+						<input type="password" name="new_pass" class="form-control" placeholder="Enter your new password">
+					</div>
+					<div class="form-group">
+						<input type="password" name="confirm_pass" class="form-control mb-2" placeholder="Confirm your new password">
+					</div>
+					<div class="form-group">
+						<button type="submit" name="submit" class="btn btn-outline-primary">Submit</button>
+					</div>
+				</form>
+			</div>
 		</div>
+	</div>
 
 	</div>
 </div>
--- user_profile_device_map.php
+++ user_profile_device_map.php
@@ -7,45 +7,45 @@
 
 	<div class="col-md-3">
     <div class="card card-primary">
-            <div class="card-header">
-              <h3 class="card-title">Mapped Device</h3>
-              <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-                  <i class="fas fa-minus"></i></button>
+        <div class="card-header">
+          <h3 class="card-title">Mapped Device</h3>
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+              <i class="fas fa-minus"></i></button>
+          </div>
+        </div>
+        <div class="card-body">
+          <form method="POST">
+              <table class="table table-sm text-nowrap">
+
+              <?php
+                $result = $user->user_profile_device_fetch($_GET['user_id']);
+                $row = mysqli_fetch_assoc($result);
+                foreach ($result as $key => $row) :
+              ?>
+                <tbody>
+                  <tr>
+                    <td><?php echo $row['device_mso'].' - '.$row['device_no']; ?></td>
+                    <td><a href="user_profile_device_release.php?assign_id=<?php echo $row['assign_id']; ?>&user_id=<?php echo $_GET['user_id']; ?>" onclick="return confirm('Do you want to release this device?');"><i class="fas fa-minus-circle"></i></a></td>
+                  </tr>
+                </tbody>
+              
+              <?php
+                endforeach;
+              ?>
+            </table>
+              <div class="form-group col-md">
+                  <input type="text" name="device_no" id="myInput" placeholder="Enter Device ID" class="form-control" required> 
               </div>
-            </div>
-            <div class="card-body">
-              <form method="POST">
-                  <table class="table table-sm text-nowrap">
 
-                  <?php
-                    $result = $user->user_profile_device_fetch($_GET['user_id']);
-                    $row = mysqli_fetch_assoc($result);
-                    foreach ($result as $key => $row) :
-                  ?>
-                    <tbody>
-                      <tr>
-                        <td><?php echo $row['device_mso'].' - '.$row['device_no']; ?></td>
-                        <td><a href="user_profile_device_release.php?assign_id=<?php echo $row['assign_id']; ?>&user_id=<?php echo $_GET['user_id']; ?>" onclick="return confirm('Do you want to release this device?');"><i class="fas fa-minus-circle"></i></a></td>
-                      </tr>
-                    </tbody>
-                  
-                  <?php
-                    endforeach;
-                  ?>
-                </table>
-                  <div class="form-group col-md">
-                      <input type="text" name="device_no" id="myInput" placeholder="Enter Device ID" class="form-control" required> 
-                  </div>
-
-                  <div class="form-group col-md">
-                    <input type="hidden" name="user_id" value="<?php echo $_GET['user_id']; ?>">
-                    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-                    <a href="device_entry.php?user_id=<?php echo $_GET['user_id']; ?>" class="btn btn-success">Add Device</a>
-                  </div>
-              </form>
-            </div>
-		    </div>
+              <div class="form-group col-md">
+                <input type="hidden" name="user_id" value="<?php echo $_GET['user_id']; ?>">
+                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                <a href="device_entry.php?user_id=<?php echo $_GET['user_id']; ?>" class="btn btn-success">Add Device</a>
+              </div>
+          </form>
+        </div>
+    </div>
 	</div>
 	<div class="col-md-6">
       <div class="card card-primary">
